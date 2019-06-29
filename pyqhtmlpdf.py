20f3cff2f47b21bb9ec0e8163ed2488fd9f722b9
Vincent Ardisson <vardisson@budget-insight.com>
2019-06-29 17:14:13 +0200

    use 4 spaces, fix pyflakes and add .editorconfig

--- pyqhtmlpdf.py
+++ pyqhtmlpdf.py
@@ -4,56 +4,54 @@ from argparse import ArgumentParser
 import os
 import sys
 
-
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtNetwork import QNetworkCookie
-from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtWidgets import (
-	QApplication,
+    QApplication,
 )
 from PyQt5.QtWebEngine import QtWebEngine
 from PyQt5.QtCore import (
-	QUrl, QEventLoop, QCoreApplication,
+    QUrl, QEventLoop,
 )
 
 
 class App(QApplication):
-	def convert(self, url, dest, cookies):
-		page = QWebEnginePage()
+    def convert(self, url, dest, cookies):
+        page = QWebEnginePage()
 
-		qurl = QUrl(url)
-		store = page.profile().cookieStore()
-		for name, value in cookies:
-			cookie = QNetworkCookie(name.encode('utf-8'), value.encode('utf-8'))
-			cookie.setDomain(qurl.host())
-			store.setCookie(cookie)
+        qurl = QUrl(url)
+        store = page.profile().cookieStore()
+        for name, value in cookies:
+            cookie = QNetworkCookie(name.encode('utf-8'), value.encode('utf-8'))
+            cookie.setDomain(qurl.host())
+            store.setCookie(cookie)
 
-		loop = QEventLoop()
-		page.loadFinished.connect(loop.quit)
+        loop = QEventLoop()
+        page.loadFinished.connect(loop.quit)
 
-		page.load(qurl)
-		loop.exec_()
+        page.load(qurl)
+        loop.exec_()
 
-		loop = QEventLoop()
-		page.pdfPrintingFinished.connect(loop.quit)
+        loop = QEventLoop()
+        page.pdfPrintingFinished.connect(loop.quit)
 
-		page.printToPdf(os.path.abspath(dest))
-		loop.exec_()
+        page.printToPdf(os.path.abspath(dest))
+        loop.exec_()
 
 
 if __name__ == '__main__':
-	parser = ArgumentParser()
-	parser.add_argument('--cookie', action='append', default=[])
-	parser.add_argument('url')
-	parser.add_argument('dest')
-	args = parser.parse_args()
+    parser = ArgumentParser()
+    parser.add_argument('--cookie', action='append', default=[])
+    parser.add_argument('url')
+    parser.add_argument('dest')
+    args = parser.parse_args()
 
-	app = App(sys.argv)
-	QtWebEngine.initialize()
+    app = App(sys.argv)
+    QtWebEngine.initialize()
 
-	cookies = []
-	for cookiestr in args.cookie:
-		name, _, value = cookiestr.partition('=')
-		cookies.append((name, value))
+    cookies = []
+    for cookiestr in args.cookie:
+        name, _, value = cookiestr.partition('=')
+        cookies.append((name, value))
 
-	app.convert(args.url, args.dest, cookies)
+    app.convert(args.url, args.dest, cookies)
